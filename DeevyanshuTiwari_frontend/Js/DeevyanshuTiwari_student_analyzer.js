2663b158b8242fce215c13034b67d0084e20fee6
DeevyanshuTiwari <tiwarideevyanshu18@gmail.com>
2026-03-19 23:26:56 +0530

    feat: implement total marks calculation

--- DeevyanshuTiwari_frontend/Js/DeevyanshuTiwari_student_analyzer.js
+++ DeevyanshuTiwari_frontend/Js/DeevyanshuTiwari_student_analyzer.js
@@ -1,26 +1,40 @@
 
 // Given Student data
 const students = [
-{
-    name: "Lalit",
-    marks: [
-        { subject: "Math", score: 78 },
-        { subject: "English", score: 82 },
-        { subject: "Science", score: 74 },
-        { subject: "History", score: 69 },
-        { subject: "Computer", score: 88 }
-    ],
-    attendance: 82
-},
-{
-    name: "Rahul",
-    marks: [
-        { subject: "Math", score: 90 },
-        { subject: "English", score: 85 },
-        { subject: "Science", score: 80 },
-        { subject: "History", score: 76 },
-        { subject: "Computer", score: 92 }
-    ],
-    attendance: 91
-}
+    {
+        name: "Lalit",
+        marks: [
+            { subject: "Math", score: 78 },
+            { subject: "English", score: 82 },
+            { subject: "Science", score: 74 },
+            { subject: "History", score: 69 },
+            { subject: "Computer", score: 88 }
+        ],
+        attendance: 82
+    },
+    {
+        name: "Rahul",
+        marks: [
+            { subject: "Math", score: 90 },
+            { subject: "English", score: 85 },
+            { subject: "Science", score: 80 },
+            { subject: "History", score: 76 },
+            { subject: "Computer", score: 92 }
+        ],
+        attendance: 91
+    }
 ];
+
+// total marks calculation
+function calculateTotalMarks(student) {
+    let total = 0;
+
+    student.marks.forEach(sub => {
+        total += sub.score;
+    });
+
+    return total;
+}
+
+console.log("Lalit total marks :",calculateTotalMarks(students[0]));
+console.log("Rahul total marks :",calculateTotalMarks(students[1]));
