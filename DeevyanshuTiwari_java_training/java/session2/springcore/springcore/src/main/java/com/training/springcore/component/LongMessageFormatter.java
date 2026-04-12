96b15d44b453d7cd823d4f023335620aedac1510
DeevyanshuTiwari <tiwarideevyanshu18@gmail.com>
2026-04-12 13:59:00 +0530

    Add Long Message Formatter

---
+++ DeevyanshuTiwari_java_training/java/session2/springcore/springcore/src/main/java/com/training/springcore/component/LongMessageFormatter.java
@@ -0,0 +1,12 @@
+package com.training.springcore.component;
+
+import org.springframework.stereotype.Component;
+
+public class LongMessageFormatter {
+
+    public String format(String topic) {
+        return "Dear User, we wanted to provide you with a detailed update " +
+                "regarding the following topic: " + topic +
+                ". Please review at your earliest convenience. Thank you.";
+    }
+}
