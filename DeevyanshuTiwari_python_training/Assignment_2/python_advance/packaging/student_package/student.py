923eb3b93409fbf75ed5560c68b4a15541928bcb
DeevyanshuTiwari <tiwarideevyanshu18@gmail.com>
2026-06-19 21:42:09 +0530

    feat: create package with two modules

---
+++ DeevyanshuTiwari_python_training/Assignment_2/python_advance/packaging/student_package/student.py
@@ -0,0 +1,11 @@
+"""
+Student module.
+"""
+
+
+def get_student_name() -> str:
+    """
+    Return student name.
+    """
+
+    return "Deevyanshu"
---
+++ DeevyanshuTiwari_python_training/Assignment_2/python_advance/packaging/student_package/teacher.py
@@ -0,0 +1,11 @@
+"""
+Teacher module.
+"""
+
+
+def get_teacher_name() -> str:
+    """
+    Return teacher name.
+    """
+
+    return "Rahul"
